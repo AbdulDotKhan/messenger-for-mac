8ff9dc591f7da71b384efd9092836ccc9f291366
Abdul Khan <akhan552@uwo.ca>
2015-04-18 15:14:41 -0400

    Initial Commit

---
+++ messenger-for-mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  messenger-for-mac
+//
+//  Created by Abdul Khan on 4/18/15.
+//  Copyright (c) 2015 Poros. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
